696a8c8e9a259235b13a56525dea36d32079045f
Nils Durner <ndurner@googlemail.com>
2018-06-08 20:01:20 +0200

    fix notification reception

--- iToolkit/AppDelegate.m
+++ iToolkit/AppDelegate.m
@@ -25,7 +25,7 @@
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [[UNUserNotificationCenter currentNotificationCenter] setDelegate:self];
     return YES;
 }
 
